8ecd19366fbfb0ce4b42b3990f5fe7855a47aded
Aaron Gullickson <aarongullickson@gmail.com>
2023-03-18 13:21:11 -0700

    update package checking

--- check_packages.R
+++ check_packages.R
@@ -5,8 +5,13 @@
 #http://www.vikram-baliga.com/blog/2015/7/19/a-hassle-free-way-to-verify-that-r-packages-are-installed-and-loaded
 
 #add new packages to the chain here
-packages = c("here","readr","haven","tidyverse","ggplot2","lubridate","stringr",
-             "tidyr","broom","knitr","texreg","kableExtra")
+packages = c(
+  "here", # absolute requirement always
+  "knitr", # for processing quarto
+  "readr","haven", # I/O
+  "tidyverse","lubridate","broom", #tidyverse and friends
+  "texreg","gt", "kableExtra" # for table output
+)
 
 package.check <- lapply(packages, FUN = function(x) {
   if (!require(x, character.only = TRUE)) {
