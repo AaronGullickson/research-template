b98a7afac00e8468641d881d4e8b9915bfba46ef
Aaron Gullickson <aarongullickson@gmail.com>
2023-03-25 14:07:26 -0700

    Change ordering in remove_artifacts.ts to reflect comments

--- utils/remove_artifacts.ts
+++ utils/remove_artifacts.ts
@@ -7,7 +7,7 @@
 
 //remove and replace the data_constructed directory
 Deno.removeSync("data/data_constructed", { recursive: true });
+Deno.mkdir("data/data_constructed");
 
 //remove the _products directory
-Deno.mkdir("data/data_constructed");
 Deno.removeSync("_products", { recursive: true });
