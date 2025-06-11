a3cba6a95251067df076b9f741a2983d1234216f
Aaron Gullickson <aarongullickson@gmail.com>
2025-06-11 13:54:55 -0700

    Do not remove the README in the constructed data folder when rerunning project

--- utils/remove_artifacts.ts
+++ utils/remove_artifacts.ts
@@ -10,14 +10,28 @@ if (!Deno.env.get("QUARTO_PROJECT_RENDER_ALL")) {
   Deno.exit();
 }
 
-//remove and replace the data_constructed directory
+// Clean all files from data/data_constructed except README.md
 try {
-  Deno.removeSync("data/data_constructed", { recursive: true });
-  Deno.mkdir("data/data_constructed");
-} catch(error) {
-  //directory does not exist
-  Deno.mkdir("data/data_constructed");
+  for await (const entry of Deno.readDir("data/data_constructed")) {
+    if (entry.name !== "README.md") {
+      const path = `data/data_constructed/${entry.name}`;
+      const info = await Deno.lstat(path);
+      if (info.isDirectory) {
+        await Deno.remove(path, { recursive: true });
+      } else {
+        await Deno.remove(path);
+      }
+    }
+  }
+} catch (error) {
+  if (error instanceof Deno.errors.NotFound) {
+    // If the directory doesn't exist, create it
+    await Deno.mkdir("data/data_constructed", { recursive: true });
+  } else {
+    throw error;
+  }
 }
+
 
 //remove the _products directory
 try {
